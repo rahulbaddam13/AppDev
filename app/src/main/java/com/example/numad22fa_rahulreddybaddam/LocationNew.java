ffde37ecfb44071db6c9f9a8afb49e467aa34d59
rahulbaddam13 <rahulrdd007@gmail.com>
2022-10-16 22:23:33 -0400

    Updated distance , back button prompt

--- app/src/main/java/com/example/numad22fa_rahulreddybaddam/LocationNew.java
+++ app/src/main/java/com/example/numad22fa_rahulreddybaddam/LocationNew.java
@@ -1,17 +1,17 @@
 package com.example.numad22fa_rahulreddybaddam;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,36 +23,34 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LocationNew extends AppCompatActivity {
 
-    TextView tv_lat, tv_lon;
-    Switch sw_locationUpdates;
+    TextView text_latitude, text_longitude;
     Location currentLocation;
+    Location recentLocation;
     List<Location> savedLocations;
-    double distance;
-    int n;
+    float[] d;
 
     FusedLocationProviderClient fusedLocationProviderClient;
     LocationRequest locationRequest;
     LocationCallback locationCallback;
-    Button StartTracking, Reset;
-    TextView trackingStatus, testt,dist;
+
+    Button Reset;
+
+    TextView dist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_new);
-        tv_lat = findViewById(R.id.tv_lat);
-        tv_lon = findViewById(R.id.tv_lon);
-        sw_locationUpdates = findViewById(R.id.sw_locationsupdates);
-        StartTracking = (Button) findViewById(R.id.startTracking);
+        d= new float[1];
+        text_latitude = findViewById(R.id.tv_lat);
+        text_longitude = findViewById(R.id.tv_lon);
         Reset = (Button) findViewById(R.id.Reset);
-        trackingStatus = findViewById(R.id.trackingStatus);
-        testt =  findViewById(R.id.testt);
         dist = findViewById(R.id.distance);
-        dist.setText("Start Tracking");
         locationRequest = new LocationRequest();
         locationRequest.setInterval(30000);
         locationRequest.setFastestInterval(5000);
@@ -60,50 +58,38 @@ public class LocationNew extends AppCompatActivity {
         savedLocations = new ArrayList<Location>();
 
         locationCallback = new LocationCallback() {
+
             @Override
             public void onLocationResult(@NonNull LocationResult locationResult) {
                 super.onLocationResult(locationResult);
-                updateUIValues(locationResult.getLastLocation());
+                recentLocation = locationResult.getLastLocation();
+                Location.distanceBetween(currentLocation.getLatitude(),currentLocation.getLongitude(),
+                        recentLocation.getLatitude(),recentLocation.getLongitude(),d);
+                updateTextView(locationResult.getLastLocation());
             }
         };
 
-        StartTracking.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                trackingStatus.setText("Tracking in Progress");
-//                testt.setText(String.valueOf(savedLocations.get(0)));
-                startLocationUpdates();
 
-            }
-        });
         Reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                trackingStatus.setText("Tracking reset");
-                updateGPS();
+                Location.distanceBetween(currentLocation.getLatitude(),currentLocation.getLongitude(),
+                        currentLocation.getLatitude(),currentLocation.getLongitude(),d);
+                String formattedString = Arrays.toString(d)
+                        .replace(",", "")
+                        .replace("[", "")
+                        .replace("]", "")
+                        .trim();
+                dist.setText(formattedString);
+
             }
         });
 
-
-//        sw_locationUpdates.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (sw_locationUpdates.isChecked()) {
-//                    startLocationUpdates();
-//                } else {
-//                    stopLocationUpdates();
-//                }
-//            }
-//        });
-
-        updateGPS();
+        updateLocation();
 
     }
 
-    private void stopLocationUpdates() {
-        tv_lon.setText("Not Tracking");
-        tv_lat.setText("Not Tracking");
-    }
+
 
     private void startLocationUpdates() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -111,75 +97,85 @@ public class LocationNew extends AppCompatActivity {
             //    ActivityCompat#requestPermissions
         }
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
-        updateGPS();
+
+
+        updateLocation();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 99:
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
-                    updateGPS();
-                }
-                else{
-                    Toast.makeText(this,"Permission Required",Toast.LENGTH_LONG).show();
-                    finish();
+        if (requestCode == 99) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                updateLocation();
+            } else {
+                Toast.makeText(this, "Permission Required", Toast.LENGTH_LONG).show();
+                finish();
 
-                }
-                break;
+            }
         }
     }
 
 
 
-    private void updateGPS() {
+    private void updateLocation() {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
                 @Override
                 public void onSuccess(Location location) {
-                    updateUIValues(location);
+                    if (currentLocation == null ) {
+                        currentLocation = location;
+                    }
+                        updateTextView(location);
+                        startLocationUpdates();
 
-                    currentLocation = location;
-                    savedLocations.add(currentLocation);
-                    testt.setText(String.valueOf(currentLocation));
-//                    n = savedLocations.size();
-//
-//                    if(savedLocations.get(n-1)!= null) {
-//                        distance = calDistance(savedLocations.get(0).getLatitude(), savedLocations.get(0).getLongitude(),
-//                                savedLocations.get(n - 1).getLatitude(), savedLocations.get(n - 1).getLongitude());
-//                        dist.setText((int) distance);
-//                    }
+                    }
 
-                }
-            });
-        } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 99);
             }
+            );
+        } else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 99);
         }
     }
 
-    private void updateUIValues(Location location) {
-        tv_lat.setText(String.valueOf(location.getLatitude()));
-        tv_lon.setText(String.valueOf(location.getLongitude()));
-        currentLocation = location;
-        savedLocations.add(currentLocation);
-//        int n = savedLocations.size();
-//        distance = calDistance(savedLocations.get(0).getLatitude(),savedLocations.get(0).getLongitude(),
-//                savedLocations.get(n-1).getLatitude(), savedLocations.get(n-1).getLongitude());
-//        dist.setText((int) distance);
+    private void updateTextView(Location location) {
+        text_latitude.setText(String.valueOf(location.getLatitude()));
+        text_longitude.setText(String.valueOf(location.getLongitude()));
+        String formattedString = Arrays.toString(d)
+                .replace(",", "")
+                .replace("[", "")
+                .replace("]", "")
+                .trim();
+        dist.setText(formattedString);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder build = new AlertDialog.Builder(this);
+        build.setTitle("EXIT?");
+        build.setMessage("Do you wanna exit")
+                .setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog alertDialog = build.create();
+        alertDialog.show();
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+        alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+        int width = (int) (getResources().getDisplayMetrics().widthPixels*0.7);
+        int height = (int) (getResources().getDisplayMetrics().heightPixels*0.32);
+        alertDialog.getWindow().setLayout(width,height);
+
     }
 
 
-    private static final double r2d = 180.0D / 3.141592653589793D;
-    private static final double d2r = 3.141592653589793D / 180.0D;
-    private static final double d2km = 111189.57696D * r2d;
-    public static double calDistance(double lt1, double ln1, double lt2, double ln2) {
-        double x = lt1 * d2r;
-        double y = lt2 * d2r;
-        return Math.acos( Math.sin(x) * Math.sin(y) + Math.cos(x) * Math.cos(y) * Math.cos(d2r * (ln1 - ln2))) * d2km;
-    }
 }
